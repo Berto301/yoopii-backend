5419c16efff59a37701b8dc725ae9495c56243af
Roberto <ratsimandresyhajatiana@gmail.com>
2024-03-06 16:52:48 +0300

    Blockage cors - debug

--- src/main.ts
+++ src/main.ts
@@ -7,8 +7,10 @@ async function bootstrap() {
   var whitelist = [process.env.URL_FRONT];
   const app = await NestFactory.create(AppModule);
   // await app.use(cors())
+  
   await app.enableCors({
    origin: function (origin, callback) {
+      console.log({whitelist,origin,test:whitelist.indexOf(origin) !== -1})
       if (whitelist.indexOf(origin) !== -1) {
         console.log(whitelist)
         callback(null, true)
