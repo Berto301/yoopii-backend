cacca757e3b16fb365b407a2f7660c14ac604529
Roberto <ratsimandresyhajatiana@gmail.com>
2024-03-06 16:45:47 +0300

    Blockage cors - debug

--- src/main.ts
+++ src/main.ts
@@ -7,19 +7,17 @@ async function bootstrap() {
   var whitelist = [process.env.URL_FRONT];
   const app = await NestFactory.create(AppModule);
   // await app.use(cors())
-  console.log({whitelist})
   await app.enableCors({
-  //   origin: function (origin, callback) {
-  //     if (whitelist.indexOf(origin) !== -1) {
-  //       console.log("allowed cors for:", origin)
-  //       callback(null, true)
-  //     } else {
-  //       console.log("blocked cors for:", origin)
-  //       callback(new Error('Not allowed by CORS'))
-  //     }
-  // },
- // allowedHeaders: 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept, Observe',
-  origin:whitelist,
+   origin: function (origin, callback) {
+      if (whitelist.indexOf(origin) !== -1) {
+        console.log(whitelist)
+        callback(null, true)
+      } else {
+        console.log("blocked cors for:", origin)
+        callback(new Error('Not allowed by CORS'))
+      }
+  },
+  // origin:whitelist,
   methods: "GET,PUT,POST,DELETE,UPDATE,OPTIONS",
   credentials: true,
   allowedHeaders: ['Authorization', 'Content-Type',]
