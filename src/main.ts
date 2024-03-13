e4fe39e60950516c04e4607431384e8bcaaf4195
Roberto <ratsimandresyhajatiana@gmail.com>
2024-03-13 16:24:38 +0300

    problem cors

--- src/main.ts
+++ src/main.ts
@@ -1,70 +1,21 @@
-// import { NestFactory } from '@nestjs/core';
-// import { AppModule } from './app.module';
-// // import * as cors from "cors"
-// import * as dotenv from 'dotenv';
-
-// async function bootstrap() {
-//   var whitelist = [process.env.URL_FRONT];
-//   const app = await NestFactory.create(AppModule);
-//   // await app.use(cors())
-  
-//   await app.enableCors({
-//   //  origin: function (origin, callback) {
-//   //     console.log({whitelist,origin,test:whitelist.indexOf(origin) !== -1})
-//   //     if (whitelist.indexOf(origin) !== -1) {
-//   //       console.log(whitelist)
-//   //       callback(null, true)
-//   //     } else {
-//   //       console.log("blocked cors for:", origin)
-//   //       callback(new Error('Not allowed by CORS'))
-//   //     }
-//   // },
-//   // origin:true,
-//   origin: function (origin, callback) {
-//     if (!origin || whitelist.indexOf(origin) !== -1) {
-//       console.log("inside",{whitelist})
-//       callback(null, true)
-//     } else {
-//       callback(new Error(`Not allowed by CORS: origin:${origin}`))
-//     }
-//   },
-//   methods: "GET,PUT,POST,DELETE,UPDATE,OPTIONS",
-//   credentials: true,
-//   // allowedHeaders: ['Authorization', 'Content-Type',],
-//   allowedHeaders: 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept, Observe,Authorization,X-Custom-Information',
-//   optionsSuccessStatus: 200,
-//   preflightContinue: false
-//   });
-//   await app.listen(process.env.PORT);
-// }
-// bootstrap();
-
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+// import * as cors from "cors"
 import * as dotenv from 'dotenv';
 
 async function bootstrap() {
   var whitelist = [process.env.URL_FRONT];
-  const app = await NestFactory.create(AppModule);
-
-  await app.enableCors({
-    origin: function (origin, callback) {
-      // Vérifie si l'origine est dans la liste blanche ou si elle est nulle (pour les requêtes non CORS)
-      if (!origin || whitelist.indexOf(origin) !== -1) {
-        console.log("Request allowed by CORS:", origin);
-        callback(null, true);
-      } else {
-        console.log("Request blocked by CORS:", origin);
-        callback(new Error(`Not allowed by CORS: origin:${origin}`));
-      }
-    },
-    methods: "GET,PUT,POST,DELETE,UPDATE,OPTIONS",
+  const app = await NestFactory.create(AppModule,{cors:{
+    origin:whitelist,
     credentials: true,
     allowedHeaders: 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept, Observe, Authorization, X-Custom-Information',
-    optionsSuccessStatus: 200,
-    preflightContinue: false
-  });
-
+    methods: "GET,PUT,POST,DELETE,UPDATE,OPTIONS",
+     //   optionsSuccessStatus: 200,
+  //   preflightContinue: false,
+  }});
+  
+  // await app.enableCors();
+  
   await app.listen(process.env.PORT);
 }
 
